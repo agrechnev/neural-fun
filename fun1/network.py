67002034080af2901c8d5580c4ac8d790cfbf184
agrechnev <shrike4625@gmail.com>
2018-02-16 10:15:32 +0200

    network in progress

--- fun1/network.py
+++ fun1/network.py
@@ -12,9 +12,17 @@ def sigmoid(z):
     return 1.0/(1.0 + np.exp(-z))
 
 class Network:
+    """ The sigmoid neural network """
     def __init__(self, sizes):
+        """ Ctor """
         self.num_layers = len(sizes)
         self.sizes = sizes
         self.biases = [np.random.randn(y,1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x) 
             for x, y in zip(sizes[:-1], sizes[1:])]
+
+    def feedforward(self, a):
+        """ Output of the network for COLUMN input a """
+        for w, b in zip(self.weights, self.biases):
+            a = sigmoid(np.dot(w, a) + b)
+        return a
